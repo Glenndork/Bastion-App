c6c2e13cbcb9b515765b3a7c77777218ed794f27
Glenndork <glennviola32@gmail.com>
2024-03-07 15:02:47 +0800

    added redirecting to home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:bastion/pages/LandingPage.dart';
-import 'package:bastion/pages/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: "Bastion",
       debugShowCheckedModeBanner: false,
       home: LandingPage(),
--- lib/pages/LandingPage.dart
+++ lib/pages/LandingPage.dart
@@ -1,35 +1,39 @@
 import 'package:flutter/material.dart';
-
+import 'package:bastion/pages/home.dart';
 class LandingPage extends StatelessWidget {
   const LandingPage({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: const BoxDecoration(
-          color: const Color.fromARGB(255, 50, 51, 49)
-        ),
-        child: const Center(
+        color: Colors.grey, // Set the background color
+        child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Row(
-                children: [
-                  Column(
-                  // Container(
-                  //   decoration: BoxDecoration(
-                  //     color: Colors.lightGreen,
-                  //   ),
-                  //   child: Text("tanignamo"),
-                  // ),
-                  ),
-                ]
+              Image.asset(
+                'assets/logo/bastionwhite.png', // Updated image path
+                width: 300, // Set the width of the image
+                height: 300, // Set the height of the image
+              ),
+              const SizedBox(
+                height: 20
+              ), // Add some spacing
+              
+              ElevatedButton(
+                child: Text('Continue'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => home()),
+                    );
+                },
               ),
             ],
           ),
         ),
-      )
-      
+      ),
     );
   }
 
