11f641db99ccc6412e71464cd8589b0c9bc999a8
Glenndork <glennviola32@gmail.com>
2024-03-11 00:27:22 +0800

    Changes in HomePage.dart

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -3,23 +3,43 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Adjust as needed
+    return Scaffold(
+      body: Container(
+        color: Colors.grey,
+        child: ThreeColumnsRow(),
+      ),
+    );
+  }
+}
+
+class ThreeColumnsRow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
       children: [
-        Column(
-          children: [
-            Text('Column 1'),
-          ],
+        Expanded(
+          child: Container(
+            height: double.infinity, // Maximum height for Column 1
+            child: Center(
+              child: Text('Column 1'),
+            ),
+          ),
         ),
-        Column(
-          children: [
-            Text('Column 2'),
-          ],
+        Expanded(
+          child: Container(
+            height: double.infinity, // Maximum height for Column 1
+            child: Center(
+              child: Text('Column 2'),
+            ),
+          ),
         ),
-        Column(
-          children: [
-            Text('Column 3'),
-          ],
+        Expanded(
+          child: Container(
+            height: double.infinity, // Maximum height for Column 1
+            child: Center(
+              child: Text('Column 3'),
+            ),
+          ),
         ),
       ],
     );
